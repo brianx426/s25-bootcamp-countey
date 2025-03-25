15f04f60863a7b69aefd976b937b831d64f28d30
Brian Liu <brianx@unc.edu>
2025-03-25 15:03:33 -0400

    initial commit

---
+++ Countey/Countey/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Countey
+//
+//  Created by Brian Liu on 3/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Countey/Countey/CounteyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounteyApp.swift
+//  Countey
+//
+//  Created by Brian Liu on 3/25/25.
+//
+
+import SwiftUI
+
+@main
+struct CounteyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
